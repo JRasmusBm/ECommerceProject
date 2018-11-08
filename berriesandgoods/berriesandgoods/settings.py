e4957fba2f1c12fc4df3a666ad0942c722eeeb39
jrasmusbm <JRasmusBm@gmail.com>
2018-11-08 19:41:45 +0100

    Install about

--- berriesandgoods/berriesandgoods/settings.py
+++ berriesandgoods/berriesandgoods/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['lab.giffeln.se']
 # Application definition
 
 INSTALLED_APPS = [
+    'about'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
