d12e588c5d1df6628b1970b8d7411dee2d301164
jrasmusbm <JRasmusBm@gmail.com>
2018-11-17 09:13:23 +0100

    blacken files

--- berriesandgoods/about/views.py
+++ berriesandgoods/about/views.py
@@ -4,7 +4,5 @@ from django.shortcuts import render
 def index(request):
     context = {"page_title": "About"}
     return render(
-        request=request,
-        template_name="about/index.html",
-        context=context,
+        request=request, template_name="about/index.html", context=context
     )
