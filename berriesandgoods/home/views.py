c1167381df4d5da6de33b1dce108c10f6b770e1e
jrasmusbm <JRasmusBm@gmail.com>
2018-11-17 09:15:42 +0100

    blacken files

--- berriesandgoods/home/views.py
+++ berriesandgoods/home/views.py
@@ -4,7 +4,5 @@ from django.shortcuts import render
 def index(request):
     context = {"page_title": "Home"}
     return render(
-        request=request,
-        template_name="home/index.html",
-        context=context,
+        request=request, template_name="home/index.html", context=context
     )
