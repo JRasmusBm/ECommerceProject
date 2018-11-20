8c6fa737bed2e905eeac1fb55a1a11d58f2de6e3
jrasmusbm <JRasmusBm@gmail.com>
2018-11-20 11:24:15 +0100

    Working on details page

--- berriesandgoods/berryBase/views.py
+++ berriesandgoods/berryBase/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
+from home.models import Berry
 
 items = [
     {
-        "name": f"Berry{i}",
+        "name": berry.nameberry,
         "price": format(round(0.20 + (0.20 * (i % 5)), 2), ".2f"),
         "unit": "kg",
-        "item_id": i,
+        "item_id": berry.idberry,
     }
-    for i in range(100)
+    for i, berry in enumerate(Berry.objects.raw("SELECT * FROM berry"))
 ]
 
 
@@ -19,7 +20,12 @@ def index(request):
 
 
 def details(request, item_id):
-    context = {"page_title": "Details", "item": items[item_id]}
+    context = {
+        "page_title": "Details",
+        "item": Berry.objects.raw(
+            f"SELECT * FROM berry WHERE idBerry={item_id}"
+        )[0],
+    }
     return render(
         request=request,
         template_name="berries/details.html",
