735daf27610fbbbc060f228f497250cd156a901e
jrasmusbm <JRasmusBm@gmail.com>
2018-11-22 23:55:20 +0100

    Add responsiveness

--- berriesandgoods/berryBase/views.py
+++ berriesandgoods/berryBase/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from home.models import Berry
 
-items = [
-    {
-        "name": berry.nameberry,
-        "price": format(round(0.20 + (0.20 * (i % 5)), 2), ".2f"),
-        "unit": "kg",
-        "item_id": berry.idberry,
-    }
-    for i, berry in enumerate(Berry.objects.raw("SELECT * FROM berry"))
-]
-
 
 def index(request):
-    context = {"page_title": "Berries", "items": items}
+    context = {
+        "page_title": "Berries",
+        "items": [
+            {
+                "name": berry.nameberry,
+                "price": format(round(0.20 + (0.20 * (i % 5)), 2), ".2f"),
+                "unit": "kg",
+                "item_id": berry.idberry,
+            }
+            for i, berry in enumerate(
+                Berry.objects.raw("SELECT * FROM berry")
+            )
+        ],
+    }
     return render(
         request=request, template_name="berries/index.html", context=context
     )
