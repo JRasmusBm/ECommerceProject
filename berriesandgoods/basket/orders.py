c6a090c9231405e4865fb4421086bbe4dbdd93ab
giffeln <lime.posklilja@hotmail.com>
2018-11-29 22:14:16 +0100

    add products to order

--- berriesandgoods/basket/orders.py
+++ berriesandgoods/basket/orders.py
@@ -2,52 +2,51 @@
 from home.models import Users
 from home.models import Orders
 from home.models import Orderitems
+from home.models import Product
 
 class OrdersBackend:
 
-    """connect order to user and add first product"""
-    def createOrder (self, idUsers):
-        o = Orders(payment=false, status=false, price=0, idusers=idUsers)
-        oi = Orderitems(amount=0, priceorderitems=0, idorders=order.idorders)
-        o.
-        oi.save()
-        return o.idorders
+    def createOrder (self, userId): #connect new order to user
+        o = Orders(payment=False, status=False, price=0, idusers=userId)
+        o.save()
+        return o
 
-
-        """add product"""
-    def addProduct (self, username, product):
+    def addProduct (self, username, productId): #add product to order
         user = Users.objects.get(email=username)
-        userOrders = []
-        for order in Orders.idusers=user.idusers:"""TODO"""
-            userOrders.append(order.idOrders)
+        product = Product.objects.get(idproduct=productId)
+        userOrder = Orders.objects.filter(idusers=user.idusers)
+        userOrder = userOrder.objects.filter(payment=False)
+        if not userOrder.exists():
+            userOrder = self.createOrder(user.idusers)
+        userItems = Orderitems.objects.filter(idproduct=productId)
+        userItem = userItems.objects.filter(idusers=user.idusers)
+        if userItem.exists():
+            userItem.amount = userItem.amount + 1
+            userItem.save()
+        else:
+            userItem = Orderitems(amount=1, idproduct=productId, idorders=userOrder.idorders, priceorderitems=product.priceproduct)
+            userItem.save()
+        userOrder.price = userOrder.price + product.priceproduct
+        userOrder.save()
 
-            """add product to orderItems"""
-        """else:"""
-            """idOrders = createOrder(idUsers)"""
-            """add product to orderItems"""        
+
+    def removeProduct (self, orderId, productId): #remove product
+        #lookup order with idOrders
+        #remove product from orderItems
+        #TODO change the order price
         pass
 
+    def pay (self, orderId): #set order to payed
+        order = Orders.objects.get(idOrders=orderId)
+        order.payment = True
+        order.save()
+        
+    def handle (self, orderId): #set order to handeled
+        order = Orders.objects.get(idOrders=orderId)
+        order.status = True
+        order.save()
 
-        """remove product"""
-    def removeProduct (self, idOrders, product):
-        """lookup order with idOrders"""
-        """remove product from orderItems"""
-        pass
-
-
-        """set pay to true"""
-    def pay (self, idOrders):
-        """lookup order from idOrders"""
-        pass
-
-
-        """set handeled to true"""
-    def handle (self, idOrders):
-        """lookup order from idOrders"""
-        pass
-
-
-        """change amount of a product"""
-    def changeAmount (self, idOrders, product, amount):
-        """lookup order from idOrders"""
+    def changeAmount (self, idOrders, product, amount): #change amount of a product
+        #lookup order from idOrders
+        #TODO change the order price
         pass
