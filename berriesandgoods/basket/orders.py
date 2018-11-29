43ff54e85e92757a71645da2256db23a739080ef
giffeln <lime.posklilja@hotmail.com>
2018-11-29 14:56:36 +0100

    create order

--- berriesandgoods/basket/orders.py
+++ berriesandgoods/basket/orders.py
@@ -1,40 +1,53 @@
 
-
+from home.models import Users
 from home.models import Orders
 from home.models import Orderitems
 
 class OrdersBackend:
-    def createOrder (self, idUsers, product):
-        """connect order to user and add first product"""
-        """create order"""
-        """add id to order"""
-        """create orderitems connected to orders"""
-        """add product to orderitems"""
-        pass
 
-    def addProduct (self, idOrders, product):
+    """connect order to user and add first product"""
+    def createOrder (self, idUsers):
+        o = Orders(payment=false, status=false, price=0, idusers=idUsers)
+        oi = Orderitems(amount=0, priceorderitems=0, idorders=order.idorders)
+        o.
+        oi.save()
+        return o.idorders
+
+
         """add product"""
-        """lookup order from idOrders"""
-        """add product to orderItems"""
+    def addProduct (self, username, product):
+        user = Users.objects.get(email=username)
+        userOrders = []
+        for order in Orders.idusers=user.idusers:"""TODO"""
+            userOrders.append(order.idOrders)
+
+            """add product to orderItems"""
+        """else:"""
+            """idOrders = createOrder(idUsers)"""
+            """add product to orderItems"""        
         pass
 
-    def removeProduct (self, idOrders, product):
+
         """remove product"""
-        """lookup order from idOrders"""
+    def removeProduct (self, idOrders, product):
+        """lookup order with idOrders"""
         """remove product from orderItems"""
         pass
 
-    def pay (self, idOrders):
+
         """set pay to true"""
+    def pay (self, idOrders):
         """lookup order from idOrders"""
         pass
 
-    def handle (self, idOrders):
+
         """set handeled to true"""
+    def handle (self, idOrders):
         """lookup order from idOrders"""
         pass
 
-    def changeAmount (self, idOrders, product, amount):
+
         """change amount of a product"""
+    def changeAmount (self, idOrders, product, amount):
         """lookup order from idOrders"""
         pass
