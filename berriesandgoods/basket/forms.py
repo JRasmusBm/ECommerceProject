3659d799018fabd2d75b8bc44638760a5f613447
jrasmusbm <JRasmusBm@gmail.com>
2018-12-06 15:14:54 +0100

    Apply flake8

--- berriesandgoods/basket/forms.py
+++ berriesandgoods/basket/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
+
 class ChangeAmount(forms.Form):
     amount = forms.IntegerField(label="", min_value=1)
--- berriesandgoods/home/migrations/0001_initial.py
+++ berriesandgoods/home/migrations/0001_initial.py
@@ -10,84 +10,136 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Orderitems',
+            name="Orderitems",
             fields=[
-                ('idorderitems', models.IntegerField(primary_key=True, serialize=False)),
-                ('amount', models.IntegerField()),
-                ('priceorderitems', models.IntegerField(db_column='priceOrderItems')),
+                (
+                    "idorderitems",
+                    models.IntegerField(primary_key=True, serialize=False),
+                ),
+                ("amount", models.IntegerField()),
+                (
+                    "priceorderitems",
+                    models.IntegerField(db_column="priceOrderItems"),
+                ),
             ],
-            options={
-                'db_table': 'orderitems',
-                'managed': False,
-            },
+            options={"db_table": "orderitems", "managed": False},
         ),
         migrations.CreateModel(
-            name='Orders',
+            name="Orders",
             fields=[
-                ('idorders', models.IntegerField(primary_key=True, serialize=False)),
-                ('payment', models.BooleanField()),
-                ('status', models.BooleanField()),
-                ('price', models.IntegerField(blank=True, null=True)),
+                (
+                    "idorders",
+                    models.IntegerField(primary_key=True, serialize=False),
+                ),
+                ("payment", models.BooleanField()),
+                ("status", models.BooleanField()),
+                ("price", models.IntegerField(blank=True, null=True)),
             ],
-            options={
-                'db_table': 'orders',
-                'managed': False,
-            },
+            options={"db_table": "orders", "managed": False},
         ),
         migrations.CreateModel(
-            name='Product',
+            name="Product",
             fields=[
-                ('idproduct', models.IntegerField(primary_key=True, serialize=False)),
-                ('nameproduct', models.CharField(max_length=45)),
-                ('priceproduct', models.IntegerField(blank=True, null=True)),
-                ('img', models.CharField(blank=True, max_length=200)),
+                (
+                    "idproduct",
+                    models.IntegerField(primary_key=True, serialize=False),
+                ),
+                ("nameproduct", models.CharField(max_length=45)),
+                (
+                    "priceproduct",
+                    models.IntegerField(blank=True, null=True),
+                ),
+                ("img", models.CharField(blank=True, max_length=200)),
             ],
-            options={
-                'db_table': 'product',
-                'managed': False,
-            },
+            options={"db_table": "product", "managed": False},
         ),
         migrations.CreateModel(
-            name='Producttype',
+            name="Producttype",
             fields=[
-                ('nameproducttype', models.CharField(max_length=45, primary_key=True, serialize=False)),
-                ('unit', models.CharField(max_length=45)),
+                (
+                    "nameproducttype",
+                    models.CharField(
+                        max_length=45, primary_key=True, serialize=False
+                    ),
+                ),
+                ("unit", models.CharField(max_length=45)),
             ],
-            options={
-                'db_table': 'producttype',
-                'managed': False,
-            },
+            options={"db_table": "producttype", "managed": False},
         ),
         migrations.CreateModel(
-            name='Users',
+            name="Users",
             fields=[
-                ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
-                ('idusers', models.AutoField(primary_key=True, serialize=False, verbose_name='idusers')),
-                ('admin', models.BooleanField(default=False, null=True)),
-                ('display_name', models.CharField(default='', max_length=255, null=True, unique=True)),
-                ('email', models.EmailField(max_length=255, unique=True, verbose_name='email address')),
+                (
+                    "password",
+                    models.CharField(
+                        max_length=128, verbose_name="password"
+                    ),
+                ),
+                (
+                    "last_login",
+                    models.DateTimeField(
+                        blank=True, null=True, verbose_name="last login"
+                    ),
+                ),
+                (
+                    "idusers",
+                    models.AutoField(
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="idusers",
+                    ),
+                ),
+                ("admin", models.BooleanField(default=False, null=True)),
+                (
+                    "display_name",
+                    models.CharField(
+                        default="", max_length=255, null=True, unique=True
+                    ),
+                ),
+                (
+                    "email",
+                    models.EmailField(
+                        max_length=255,
+                        unique=True,
+                        verbose_name="email address",
+                    ),
+                ),
             ],
-            options={
-                'db_table': 'users',
-                'managed': True,
-            },
+            options={"db_table": "users", "managed": True},
         ),
         migrations.CreateModel(
-            name='Review',
+            name="Review",
             fields=[
-                ('idusers', models.ForeignKey(db_column='idusers', on_delete=django.db.models.deletion.DO_NOTHING, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
-                ('comment', models.CharField(blank=True, max_length=8000, null=True)),
-                ('rating', models.IntegerField(validators=[django.core.validators.MinValueValidator(1), django.core.validators.MaxValueValidator(5)])),
+                (
+                    "idusers",
+                    models.ForeignKey(
+                        db_column="idusers",
+                        on_delete=django.db.models.deletion.DO_NOTHING,
+                        primary_key=True,
+                        serialize=False,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+                (
+                    "comment",
+                    models.CharField(
+                        blank=True, max_length=8000, null=True
+                    ),
+                ),
+                (
+                    "rating",
+                    models.IntegerField(
+                        validators=[
+                            django.core.validators.MinValueValidator(1),
+                            django.core.validators.MaxValueValidator(5),
+                        ]
+                    ),
+                ),
             ],
-            options={
-                'db_table': 'review',
-                'managed': False,
-            },
+            options={"db_table": "review", "managed": False},
         ),
     ]
